8a431696fee5e9cbf217269e1d926c8ae416ed06
cicatriz <ryan@geknowm.com>
2013-07-27 21:58:02 +0800

    move actionmailer settings to production config and add default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Mailgregate::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'mailgregate.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'mailgregate.herokuapp.com' }
 end
