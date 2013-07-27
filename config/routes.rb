62a01fc1dd7b2abd7159a45530b6d0645de2ad2f
cicatriz <ryan@geknowm.com>
2013-07-27 20:23:48 +0800

    rails_apps_composer: models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mailgregate::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
