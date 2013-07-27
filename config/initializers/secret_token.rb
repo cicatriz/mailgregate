47d4127ea84258ea48b0f8d9c6ece03f44435ecf
cicatriz <ryan@geknowm.com>
2013-07-27 20:12:23 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailgregate::Application.config.secret_key_base = 'b67b33a797042d721097a8f58bd294fcf36210d1387c6d9bed240d099a6f99041fde87b98201d6574eba44377e77afb4545ede83e87ff773d8c7c234aa033efb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mailgregate::Application.config.session_store :cookie_store, key: '_mailgregate_session'
