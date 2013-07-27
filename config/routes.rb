5b22b97273357b21ee854d2a5a5011352da838ca
cicatriz <ryan@geknowm.com>
2013-07-27 20:23:51 +0800

    rails_apps_composer: controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mailgregate::Application.routes.draw do
+  get "home/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
